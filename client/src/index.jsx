6c3f997ea9bf9d77be621417eeb61d86ed223c8b
christophe <christophe.vandevoir@epitech.eu>
2025-04-30 21:05:42 +0200

    [+] add entry point index.jsx with root rendering and performance reporting

---
+++ client/src/index.jsx
@@ -0,0 +1,29 @@
+/**
+ * ┌────────────────────────────────────────────────────────────────────────────
+ * │ @author                    Christophe Vandevoir
+ * ├────────────────────────────────────────────────────────────────────────────
+ * │ @file           index.jsx
+ * │ @path          client/src/index.jsx
+ * │ @description   index implementation
+ * │ @version       1.0.0
+ * │
+ * │ @email         christophe.vandevoir@epitech.eu
+ * │ @date          2025-04-30
+ * ├────────────────────────────────────────────────────────────────────────────
+ * │ @copyright     (c) 2025 Christophe Vandevoir
+ * └────────────────────────────────────────────────────────────────────────────
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
+reportWebVitals();
