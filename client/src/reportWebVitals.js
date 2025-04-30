996d64fdc093939bda3596a845d3018579ce00c1
christophe <christophe.vandevoir@epitech.eu>
2025-04-30 21:10:27 +0200

    [+] add reportWebVitals utility for performance measurement

---
+++ client/src/reportWebVitals.js
@@ -0,0 +1,29 @@
+/**
+ * ┌────────────────────────────────────────────────────────────────────────────
+ * │ @author                    Christophe Vandevoir
+ * ├────────────────────────────────────────────────────────────────────────────
+ * │ @file           reportWebVitals.js
+ * │ @path          client/src/reportWebVitals.js
+ * │ @description   reportWebVitals implementation
+ * │ @version       1.0.0
+ * │
+ * │ @email         christophe.vandevoir@epitech.eu
+ * │ @date          2025-04-30
+ * ├────────────────────────────────────────────────────────────────────────────
+ * │ @copyright     (c) 2025 Christophe Vandevoir
+ * └────────────────────────────────────────────────────────────────────────────
+ */
+
+const reportWebVitals = (onPerfEntry) => {
+    if (onPerfEntry && onPerfEntry instanceof Function) {
+        import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+            getCLS(onPerfEntry);
+            getFID(onPerfEntry);
+            getFCP(onPerfEntry);
+            getLCP(onPerfEntry);
+            getTTFB(onPerfEntry);
+        });
+    }
+};
+
+export default reportWebVitals;
